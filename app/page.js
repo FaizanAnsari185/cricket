5497dd121157a9e4f6a46dd6b49e2e5b38f7f93e
FaizanAnsari185 <superseupermfa@gmail.com>
2024-06-24 13:48:09 +0530

    add team 2 score

--- app/page.js
+++ app/page.js
@@ -3,23 +3,36 @@ import Playbutton from "@/components/playbutton";
 import Run from "@/components/run";
 import React, { useState } from "react";
 
+const MAX_OVERS = 2;
+
 const page = () => {
   const [runInOneOver, setRunInOneOver] = useState([]);
   const [ballInOneOver, setBallInOneOver] = useState(0);
   const [run, setRun] = useState(0);
   const [wicket, setWicket] = useState(0);
   const [over, setOver] = useState(0);
+  // Team One State
   const [teamOneOver, setTeamOneOver] = useState(0);
-  const [teamBallInOneOver, setTeamBallInOneOver] = useState(0);
+  const [teamOneBallInOneOver, setTeamOneBallInOneOver] = useState(0);
   const [teamOneRun, setTeamOneRun] = useState(0);
-  const [teamOneWicket, setTeamOnewWicket] = useState(0);
-  const MAX_OVERS = 2;
+  const [teamOneWicket, setTeamOneWicket] = useState(0);
+  // Team Two State
+  const [teamTwoOver, setTeamTwoOver] = useState(0);
+  const [teamTwoBallInOneOver, setTeamTwoBallInOneOver] = useState(0);
+  const [teamTwoRun, setTeamTwoRun] = useState(0);
+  const [teamTwoWicket, setTeamTwoWicket] = useState(0);
+  
   function updateRun(pb) {
-    if (wicket === 10 || over === MAX_OVERS) {
+    if (wicket === 9 || over === MAX_OVERS) {
       setTeamOneRun(run);
-      setTeamOnewWicket(wicket);
+      setTeamOneWicket(wicket);
       setTeamOneOver(over);
-      setTeamBallInOneOver(ballInOneOver);
+      setTeamOneBallInOneOver(ballInOneOver);
+
+      setTeamTwoRun(run);
+      setTeamTwoWicket(wicket);
+      setTeamTwoOver(over);
+      setTeamTwoBallInOneOver(ballInOneOver);
 
       setRun(0);
       setWicket(0);
@@ -31,15 +44,7 @@ const page = () => {
     if (ballInOneOver === 6) {
       return;
     }
-    // if (ballInOneOver === 5 && over === MAX_OVERS - 1) {
-    //   setTeamOneRun(run);
-    //   setTeamOnewWicket(wicket);
-    //   setTeamOneOver(over);
-    //   setTeamBallInOneOver(ballInOneOver);
-      
-      
-     
-    // }
+    
     if (pb === "Dot Ball") {
       setRunInOneOver([...runInOneOver, "*"]);
       setBallInOneOver(ballInOneOver + 1);
@@ -66,6 +71,53 @@ const page = () => {
       setRun(run + pb);
     }
   }
+
+    // function updateRun(pb) {
+    //   if (wicket === 9 || over === MAX_OVERS) {
+    //   setTeamTwoRun(run);
+    //   setTeamTwoWicket(wicket);
+    //   setTeamTwoOver(over);
+    //   setTeamTwoBallInOneOver(ballInOneOver);
+  
+    //   setRun(0);
+    //   setWicket(0);
+    //   setOver(0);
+    //   setBallInOneOver(0);
+    //   setRunInOneOver([]);
+    //   return;
+    //   }
+    //   if (ballInOneOver === 6) {
+    //     return;
+    //   }
+  
+    //   if (pb === "Dot Ball") {
+    //     setRunInOneOver([...runInOneOver, "*"]);
+    //     setBallInOneOver(ballInOneOver + 1);
+    //     return;
+    //   }
+    //   if (pb === "Wicket") {
+    //     setRunInOneOver([...runInOneOver, "W"]);
+    //     setBallInOneOver(ballInOneOver + 1);
+    //     setWicket(wicket + 1);
+    //     return;
+    //   }
+    //   if (pb === "No Ball") {
+    //     setRunInOneOver([...runInOneOver, "NB"]);
+    //     setRun(run + 1);
+    //     return;
+    //   }
+    //   if (pb === "Wide Ball") {
+    //     setRunInOneOver([...runInOneOver, "WB"]);
+    //     setRun(run + 1);
+    //     return;
+    //   } else {
+    //     setRunInOneOver([...runInOneOver, pb]);
+    //     setBallInOneOver(ballInOneOver + 1);
+    //     setRun(run + pb);
+    //   }
+    // }
+
+    
 
   function updateOver() {
     setRunInOneOver([]);
@@ -102,10 +154,14 @@ const page = () => {
           <div className="flex justify-center">
             Current Score: {run}-{wicket} / {over + "." + ballInOneOver}
           </div>
-          {(teamOneOver === MAX_OVERS || teamOneWicket === 10) && (
-            <div className="flex justify-center">
-              Team 1 Total Score: {teamOneRun}-{teamOneWicket} /{" "}
-              {teamOneOver + "." + teamBallInOneOver}
+          {(teamOneOver === MAX_OVERS || teamOneWicket === 9) && (
+            <div className="flex justify-start text-red-700">
+              Team 1 Total Score: {teamOneRun}-{teamOneWicket} /{" "}{teamOneOver + "." + teamOneBallInOneOver}
+            </div>
+          )}
+          {(teamTwoOver === MAX_OVERS || teamTwoWicket === 9) && (
+            <div className="flex justify-end text-amber-700">
+              Team 2 Total Score: {teamTwoRun}-{teamTwoWicket} /{" "}{teamTwoOver + "." + teamTwoBallInOneOver}
             </div>
           )}
         </div>
@@ -115,3 +171,17 @@ const page = () => {
 };
 
 export default page;
+
+
+// if (ballInOneOver === 5 && over === MAX_OVERS - 1) {
+    //   setTeamOneRun(run);
+    //   setTeamOneWicket(wicket);
+    //   setTeamOneOver(over);
+    //   setTeamOneBallInOneOver(ballInOneOver);
+    // }
+
+    // if (wicket === "9") {
+    //   setTeamOneWicket(teamOneWicket + 1);
+    //   setTeamOneBallInOneOver(teamOneBallInOneOver + 1);
+    //   return;
+    // }
